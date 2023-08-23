0955db7c34e9c28e621277e5e4be53bc4c52cffa
ewen-r <ewen.reynolds@gmail.com>
2023-08-23 13:52:19 +0100

    Better implementation of QuestionComponent. Now correctly assesses given answers. Better formatting.

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -12,6 +12,8 @@ import { NGXLogger } from 'ngx-logger';
 import { QuestionType } from 'src/app/shared/interfaces/enums';
 import { Question } from 'src/app/shared/interfaces/interfaces';
 
+
+/** A unique ID number which is incremented for each instance of this component. */
 let uniqueId: number = 0;
 
 /** Component to add/modify a question.
@@ -26,7 +28,7 @@ export class QuestionComponent implements OnInit {
   /** Name of this component. */
   readonly COMPONENT_NAME = 'QuestionComponent';
 
-  /** Unique prefix for ID and name of HTML Elements in this component.
+  /** Unique prefix for id and name of HTML Elements in this component.
     * This prevents unwanted side effects when there are clashes.
     * e.g when the component is included more than one time on the page.
     * */
@@ -41,14 +43,15 @@ export class QuestionComponent implements OnInit {
   /** Outputs whether user was correct or incorrect. */
   @Output() result: EventEmitter<boolean> = new EventEmitter();
 
-  /** If true.. shows answerDiv */
-  showAnswer: boolean = false;
+  /** The correct answer to this question. */
+  correctAnswerMessage: string = '';
 
-  /** User's answer to this question. */
-  userAnswer: boolean | null = null;
+  /** Set to true when user submits their answer */
+  answerSubmitted: boolean = false;
 
-  /** Displayed answer to this question. */
-  displayAnswer: string = '';
+  /** Whether user's submitted answer is correct.
+    * Initially null until an answer is submitted. */
+  answerSubmittedResult: boolean | null = null;
 
   /** Enum accessor method for QuestionType (Allows enum to be used in template). */
   public get QuestionType(): typeof QuestionType {
@@ -68,22 +71,32 @@ export class QuestionComponent implements OnInit {
   */
   ngOnInit() {
     this.logger.log(`${this.COMPONENT_NAME}: ngOnInit():`);
-
-    if (this.question) {
-      this.displayAnswer = `${this.question.answer.match}  `;
-      if (this.question.answer.statement) {
-        this.displayAnswer += `(${this.question.answer.statement})`;
-      }
-    }
   }
 
 
-  /** Handle onAnswerButton button. */
-  onAnswerButton() {
-    this.userAnswer = !this.userAnswer;
-    this.logger.debug(`${this.COMPONENT_NAME}: onAnswerButton():`, this.userAnswer);
-    this.result.emit(this.userAnswer);
-  }
+  /** Handles submit button.
+    *- Determine if answer is correct and emit result to parent.
+    * - If not a true/false question and an incorrect answer was given...
+    * - - Construct correctAnswerMessage message.
+    * - Set answerSubmitted.
+    * @param {string} value Value of submitted answer.
+  */
+  onSubmit(value: string) {
+    this.logger.debug(`${this.COMPONENT_NAME}: onSubmit():`, value);
 
+    // Determine if answer is correct and emit result to parent.
+    this.answerSubmittedResult = (value === this.question?.answer.match);
+    this.result.emit(this.answerSubmittedResult);
+
+    // If not a true/false question and an incorrect answer was given..
+    if ((this.question?.questionType !== QuestionType.TRUE_FALSE) &&
+      // Construct correctAnswerMessage message
+      !this.answerSubmittedResult) {
+      this.correctAnswerMessage = 'The correct answer is ' + String(this.question?.answer.match);
+    };
+
+    // Set answerSubmitted
+    this.answerSubmitted = true;
+  }
 
 }
